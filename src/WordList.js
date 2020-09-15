871b7304f87a32c4029cf738a70b74e27bea2abd
Peter Bond at imac <peter.bond40@gmail.com>
2020-09-14 20:39:48 -0600

    one data show works

--- src/WordList.js
+++ src/WordList.js
@@ -3,23 +3,33 @@ import React from 'react';
 import axios from 'axios';
 
 export default class WordList extends React.Component {
-    state = {
-        persons: []
+    constructor(props){
+        super(props)
+        this.state = {
+            vocab : []
+        }
     }
+    
 
     componentDidMount(){
         axios.get('http://localhost/word-app/php/vocab.php')
         .then(res => {
-            const persons = res.data;
-            this.setState({ persons });
+            const vocab = res.data;
+            this.setState({ vocab: vocab[0] });
         })
+        
     }
 
     render(){
         return (
-            <ul>
-                { this.state.persons.map(person => <li>{person.vocab}</li>)}
-            </ul>
+            <div>
+                <h2>{this.state.vocab.vocab}</h2>
+                <span>{this.state.vocab.part_of_speech}</span>
+                <span>{this.state.vocab.translation1}</span>;
+                <span>{this.state.vocab.translation2}</span>;
+                <span>{this.state.vocab.translation3}</span>
+                
+            </div>
         )
     }
 }
