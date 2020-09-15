0500365cb739d95cd6038746dc812e55152df9b5
Peter Bond at imac <peter.bond40@gmail.com>
2020-09-15 10:45:16 -0600

    css first touch, align center

--- src/WordList.js
+++ src/WordList.js
@@ -2,33 +2,36 @@ import React from 'react';
 
 import axios from 'axios';
 
+import './WordList.css';
 export default class WordList extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            vocab : []
-        }
-    }
     
+    state = {
+        vocab : []
+    }
 
     componentDidMount(){
         axios.get('http://localhost/word-app/php/vocab.php')
         .then(res => {
-            const vocab = res.data;
-            this.setState({ vocab: vocab[0] });
+            const vocab = res.data[0];
+            this.setState({ vocab: vocab });
         })
         
     }
 
     render(){
         return (
-            <div>
-                <h2>{this.state.vocab.vocab}</h2>
-                <span>{this.state.vocab.part_of_speech}</span>
-                <span>{this.state.vocab.translation1}</span>;
-                <span>{this.state.vocab.translation2}</span>;
-                <span>{this.state.vocab.translation3}</span>
-                
+            <div className="board">
+                <div className ="word-card">
+                   <div className="vocab">
+                        <span>{this.state.vocab.vocab}</span>
+                    </div>
+                    <div className="translation">
+                        <span>{this.state.vocab.part_of_speech}</span>
+                        <span>{this.state.vocab.translation1}</span>;
+                        <span>{this.state.vocab.translation2}</span>;
+                        <span>{this.state.vocab.translation3}</span>
+                    </div> 
+                </div>
             </div>
         )
     }
