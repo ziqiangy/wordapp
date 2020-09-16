f87960c37710c5462b6ad704e7913fba0026f611
Peter Bond at imac <peter.bond40@gmail.com>
2020-09-16 08:25:02 -0600

    next page last page works but still have question fetch happens before setState happens

--- src/WordList.js
+++ src/WordList.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import './WordList.css';
 export default class WordList extends React.Component {
-    
-    state = {
-        vocab : [],
-        count : 0,
-        show : false
+    constructor(props){
+        super(props)
+        this.last = this.last.bind(this)
+        this.next = this.next.bind(this)
+        this.switch = this.switch.bind(this)
+        this.state = {
+                vocab : [],
+                count : 0,
+                show : false
+            }
     }
-
+    
     componentDidMount(){
-        this.fetchNextWord()
+        this.fetchWord(0)
     }
 
-    next(){
-        this.fetchNextWord()
+    next = () => {
+        this.setState({show: false})
+        this.setState({count: this.state.count+1})
+        this.fetchWord(this.state.count+1)
+    }
+
+    last = () => {
+        this.setState({show: false})
+        this.setState({count: this.state.count-1})
+        this.fetchWord(this.state.count-1)
     }
     
-    switch(){
+    switch = () => {
         this.setState({show:!this.state.show});
     }
 
-    fetchNextWord(){
+    fetchWord = (offset) => {
+        
+        // dir? this.setState({count: this.state.count+1}) : this.setState({count: this.state.count-1})
+        // this.setState({show: false});
+        
+
         let myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
         let urlencoded = new URLSearchParams();
-        urlencoded.append("offset", this.state.count);
-
+        urlencoded.append("offset", offset);
+        
         let options = {
             method: 'POST',
             headers: myHeaders,
@@ -38,26 +56,28 @@ export default class WordList extends React.Component {
             .then(response => response.json())
             .then(result => {
                 const vocab = result[0];
-            this.setState({ vocab: vocab });
+                // console.log(result[0]);
+                // console.log(this.state.count)
+                this.setState({ vocab: vocab });
+                
             })
             .catch(error => console.log('error', error));
-
-            this.setState({count: this.state.count+1});
-            this.setState({show: false});
     }
 
     render(){
         return (
             <div className="board">
-                <div>
-                    <span onClick={()=>this.next()}>Next Page</span>
-                    <span onClick={()=>this.switch()}>{this.state.show? 'hide' : 'show'}</span>
+                <div className="word-header">
+                    <div className="last-page-button" onClick={()=>this.last()}>Last Page</div>
+                    <div className="show-hide-button" onClick={()=>this.switch()}>{this.state.show? 'hide' : 'show'}</div>
+                    <div className="next-page-button" onClick={()=>this.next()}>Next Page</div>
                 </div>
                 <div className ="word-card">
                     <div className="vocab">
                         <span>{this.state.vocab.vocab}</span>
                     </div>
                     <Translation vocab={this.state.vocab} show={this.state.show} />
+                    {/* <div><span>{this.state.count}</span></div> */}
                 </div>
             </div>
         )  
