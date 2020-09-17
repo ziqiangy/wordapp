e32f3eb1cae8c5c5ce3c5694f29c220ddd222f62
Peter Bond at imac <peter.bond40@gmail.com>
2020-09-17 10:04:49 -0600

    Notes component start showing up

---
+++ src/Notes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Notes.css'
+export default class Notes extends React.Component{
+
+    render(){
+        return(
+            <div className="notes-board">
+                
+                <div className="notes-header">
+                    
+                </div>
+                <div className ="notes-card">
+                    
+                </div>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import WordList from './WordList'
+import WordList from './WordList';
+import Notes from './Notes';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <WordList />
+    <Notes />
   </React.StrictMode>,
   document.getElementById('root')
 );
