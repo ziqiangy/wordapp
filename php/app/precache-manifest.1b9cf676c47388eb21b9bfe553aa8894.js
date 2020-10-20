710ce9eb6c3a1a377a072ffb2a1b653735e0d0e0
Peter Yuan Ubuntu <peter.bond40@gmail.com>
2020-10-20 11:58:32 -0600

    crawler working local

---
+++ php/app/precache-manifest.1b9cf676c47388eb21b9bfe553aa8894.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0380a1449d8729c318abfd49c1e6f2ff",
+    "url": "/myhomeapp/build/index.html"
+  },
+  {
+    "revision": "bb84a46ad335e72b653a",
+    "url": "/myhomeapp/build/static/css/main.30f389aa.chunk.css"
+  },
+  {
+    "revision": "ea072a556ce39c3e6777",
+    "url": "/myhomeapp/build/static/js/2.86df383a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myhomeapp/build/static/js/2.86df383a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb84a46ad335e72b653a",
+    "url": "/myhomeapp/build/static/js/main.8883240d.chunk.js"
+  },
+  {
+    "revision": "ee791d74e857a38fb695",
+    "url": "/myhomeapp/build/static/js/runtime-main.af4897ab.js"
+  },
+  {
+    "revision": "22995d316859661e6cdb9289ddd61857",
+    "url": "/myhomeapp/build/static/media/arrow-circle-left-solid.22995d31.svg"
+  },
+  {
+    "revision": "e7f0741083586757350950579a93b842",
+    "url": "/myhomeapp/build/static/media/arrow-circle-right-solid.e7f07410.svg"
+  },
+  {
+    "revision": "f6ffb171becc50da0fce1cadadd9525d",
+    "url": "/myhomeapp/build/static/media/chalkboard-solid.f6ffb171.svg"
+  },
+  {
+    "revision": "3052c53b93a4fdda198ff76ba82edf80",
+    "url": "/myhomeapp/build/static/media/cloud-solid.3052c53b.svg"
+  },
+  {
+    "revision": "9a4d5fd542b2140b088ef8beac109cf6",
+    "url": "/myhomeapp/build/static/media/edit-solid.9a4d5fd5.svg"
+  },
+  {
+    "revision": "039791567bc12ae5763ac4ef1961cae5",
+    "url": "/myhomeapp/build/static/media/external-link-alt-solid.03979156.svg"
+  },
+  {
+    "revision": "35985bde20cb1c9d3ff2c4f62d81ba1c",
+    "url": "/myhomeapp/build/static/media/eye-regular.35985bde.svg"
+  },
+  {
+    "revision": "b4eb391d5011d7b1724f778c14ba5eeb",
+    "url": "/myhomeapp/build/static/media/eye-slash-regular.b4eb391d.svg"
+  },
+  {
+    "revision": "5a0a0658ca52d104dc355426bd0165d4",
+    "url": "/myhomeapp/build/static/media/font-solid.5a0a0658.svg"
+  },
+  {
+    "revision": "1f44e83cbd55b5d942db4b7f4b956407",
+    "url": "/myhomeapp/build/static/media/list-alt-regular.1f44e83c.svg"
+  },
+  {
+    "revision": "2de4ac5c059bd49b4f32d7047e06f4d4",
+    "url": "/myhomeapp/build/static/media/plus-circle-solid.2de4ac5c.svg"
+  },
+  {
+    "revision": "59f2e752784451b4f433b1bcce02809f",
+    "url": "/myhomeapp/build/static/media/sticky-note-regular.59f2e752.svg"
+  }
+]);
